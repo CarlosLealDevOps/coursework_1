3e393b6402dec1f59d51b5fd611d01373c7d27d6
Ubuntu <CLEALDevOps@lab2VM.2itibb4ck3hupauynxpb3itmja.bx.internal.cloudapp.net>
2019-10-28 12:27:52 +0000

    added input validation to Dec2Hex

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -7,39 +7,46 @@ class Dec2Hex
 
     public static void main(String args[])
     {
-if (args != null)
-{
-	System.err.println("Please input a number");
-        if (args.length > 0) 
+	if (args != null)
+	{
+		if (args.length == 0)
+		{
+			System.err.println("Please enter an argument (e.g. 15, 6, 29)");
+			return;
+		}
+		if (args.length > 0) 
+		{
+		    try 
+		    {
+			Arg1 = Integer.parseInt(args[0]);
+		    } 
+		    catch (NumberFormatException e) 
+		    {
+			System.err.println("Argument" + args[0] + " must be an integer.");
+			System.exit(1);
+		    }
+		}
+
+
+		char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+		 int rem, num;
+		num = Arg1;
+		String hexadecimal=""; 
+		System.out.println("Converting the Decimal Value " + num + " to Hex...");
+
+
+	       while(num != 0)
+		{
+		    rem = num%16;
+		    hexadecimal = ch[rem] + hexadecimal;
+		    num = num/16;
+		}
+
+		System.out.println("Hexadecimal representation is : " + hexadecimal);
+		} 
+	else 
         {
-            try 
-            {
-                Arg1 = Integer.parseInt(args[0]);
-            } 
-            catch (NumberFormatException e) 
-            {
-                System.err.println("Argument" + args[0] + " must be an integer.");
-                System.exit(1);
-            }
-        }
-
-
-    	char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-         int rem, num;
-	num = Arg1;
-	String hexadecimal=""; 
-        System.out.println("Converting the Decimal Value " + num + " to Hex...");
-        
-
-       while(num != 0)
-        {
-	    rem = num%16;
-	    hexadecimal = ch[rem] + hexadecimal;
-            num = num/16;
-        }
-        
-        System.out.println("Hexadecimal representation is : " + hexadecimal);
-System.out.println("test");    
-} 
+		System.out.println("No input found");
+	}
+    }
 }
-} 
